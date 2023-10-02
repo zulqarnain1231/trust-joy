ab13ac06174c7f6e1550d16cd9c4a66d402e7371
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-10-01 23:06:17 -0700

    fixed the tailwind theme and added startup assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          main: "#7B71EB",
+        },
+        white: {
+          main: "#FFF",
+          off: "#E6E7FF",
+        },
+        black: {
+          main: "#000",
+          secondary: "#2E2C34",
+          off: "#4B5563",
+        },
+      },
+      fontFamily: {
+        caveat: ["Caveat", "cursive"],
+        dmSans: ["DM Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
